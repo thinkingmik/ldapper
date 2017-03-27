058b1fcc166f5d197f51503d8d27edc1f2eaa6e3
thinkingmik <michi.andreoli@gmail.com>
2017-03-27 13:09:07 +0200

    add new methods and test cases

--- test/01_methods.js
+++ test/01_methods.js
@@ -2,31 +2,41 @@ var cheerio = require('cheerio')
 var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
+var assert = chai.assert;
 var Ldapper = require('../').Ldapper;
 var Promise = require('bluebird');
 
+var entry = {
+  cn: 'test',
+  sn: 'test',
+  givenName: 'test',
+  loginShell: 'test',
+  displayName: 'test user',
+  uid: 'test',
+  uidNumber: '50623',
+  gidNumber: '0',
+  homeDirectory: '0',
+  mail: ['test@acme.com'],
+  objectclass: ['inetOrgPerson', 'posixAccount']
+};
+
+var changes = [
+  { op: 'replace', attr: 'loginShell', val: 'test.01' },
+  { op: 'delete', attr: 'carLicense', val: '1' }
+]
+
 var options = {
-  domainControllers: [''],
-  searchScope: '',
+  domainControllers: ['192.168.99.100'],
+  searchScope: 'ou=users,dc=acme,dc=com',
   tombstone: null,
   root: {
-    dn: '',
+    dn: 'cn=admin,dc=acme,dc=com',
     password: {
       crypton: true,
-      value: ''
+      value: 'uV1ju3uY1JerhQ9z/nPr2w=='
     }
   },
-  crypton: {
-    crypto: {
-          secretKey: '',
-          algorithm: 'AES-256-CBC',
-          inputEncoding: 'utf8',
-          outputEncoding: 'base64'
-      },
-      bcrypt: {
-          saltRounds: 5
-      }
-  },
+  crypton: null,
   ssl: false,
   timeout: null,
   connectTimeout: null,
@@ -35,20 +45,156 @@ var options = {
 
 var ldapper = new Ldapper(options);
 
-describe('Call Crypton cipher method', function() {
+describe('Call ldapper find method', function() {
   this.timeout(5000);
-  it('should return a ciphered text', function() {
-    return ldapper.find('(mail=*)', ['objectGUID'])
-    .then(function(res) { console.log(res); expect(res).to.exist; })
-    .catch(function(err) { console.log(err); });
+  it('should return an array of entries', function() {
+    return ldapper.find('(cn=*)', ['cn'])
+    .then(function(res) {
+      assert.isTrue(res.length > 0);
+      expect(res).to.be.instanceof(Array);
+      expect(res[0]).to.have.property('cn');
+    });
+  });
+  it('should return an empty array', function() {
+    return ldapper.find('(cn=testfake)')
+    .then(function(res) {
+      expect(res).to.be.instanceof(Array);
+      assert.isTrue(res.length == 0);
+    });
+  });
+  it('should return a LDAPSearchError', function() {
+    return ldapper.find('(cn=*)', ['cn'], 'fakeDn')
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPSearchError');
+    });
   });
 });
 
-describe('Call Crypton cipher method', function() {
+describe('Call ldapper findOne method', function() {
   this.timeout(5000);
-  it('should return a ciphered text', function() {
-    return ldapper.authenticate('', '')
-    .then(function(res) { console.log(res); expect(res).to.exist; })
-    .catch(function(err) { console.log(err); });
+  it('should return an entry', function() {
+    return ldapper.findOne('uid=joe,ou=users,dc=acme,dc=com', ['cn'])
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.have.property('cn');
+    });
+  });
+  it('should return null', function() {
+    return ldapper.findOne('uid=fake,ou=users,dc=acme,dc=com', ['cn'])
+    .then(function(res) {
+      expect(res).to.be.null;
+    });
+  });
+  it('should return a LDAPSearchError', function() {
+    return ldapper.findOne('fakeDn', ['cn'])
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPSearchError');
+    });
   });
 });
+
+describe('Call ldapper authenticate method', function() {
+  this.timeout(5000);
+  it('should return an entry', function() {
+    return ldapper.authenticate('joe', 'Password1.', 'uid', 'cn')
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.have.property('cn');
+    });
+  });
+  it('should return null', function() {
+    return ldapper.authenticate('joe', 'Password2.', 'uid', 'cn')
+    .then(function(res) {
+      expect(res).to.be.null;
+    });
+  });
+  it('should return a LDAPAuthenticationError', function() {
+    return ldapper.authenticate('joe', 'Password1.', 'uid', 'cn', 'fakeDn')
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPAuthenticationError');
+    });
+  });
+});
+/*
+describe('Call ldapper add method', function() {
+  this.timeout(5000);
+  it('should return a new entry', function() {
+    return ldapper.add('uid=test,ou=users,dc=acme,dc=com', entry)
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.have.property('dn');
+    });
+  });
+  it('should return a LDAPAddError', function() {
+    return ldapper.add('uid=test,ou=users,dc=acme,dc=org', entry)
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPAddError');
+    });
+  });
+});
+*/
+describe('Call ldapper change method', function() {
+  this.timeout(5000);
+  it('should return true', function() {
+    return ldapper.change('uid=test,ou=users,dc=acme,dc=com', changes)
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('should return a LDAPChangeError', function() {
+    return ldapper.change('uid=fake,ou=users,dc=acme,dc=com', changes)
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPChangeError');
+    });
+  });
+});
+/*
+describe('Call ldapper rename method', function() {
+  this.timeout(5000);
+  it('should return true', function() {
+    return ldapper.rename('uid=test,ou=users,dc=acme,dc=com', 'uid=testNew,ou=users,dc=acme,dc=com')
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('should return false', function() {
+    return ldapper.rename('uid=fake,ou=users,dc=acme,dc=com', 'uid=testNew,ou=users,dc=acme,dc=com')
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.be.equal(false);
+    });
+  });
+  it('should return a LDAPRenameError', function() {
+    return ldapper.rename('uid=testNew,ou=users,dc=acme,dc=com', 'uid=testNew,ou=users,dc=acme,dc=com')
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPRenameError');
+    });
+  });
+});
+
+describe('Call ldapper delete method', function() {
+  this.timeout(5000);
+  it('should return true', function() {
+    return ldapper.delete('uid=testNew,ou=users,dc=acme,dc=com')
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.be.equal(true);
+    });
+  });
+  it('should return false', function() {
+    return ldapper.delete('uid=fake,ou=users,dc=acme,dc=com')
+    .then(function(res) {
+      expect(res).to.not.be.null;
+      expect(res).to.be.equal(false);
+    });
+  });
+  it('should return a LDAPDeleteError', function() {
+    return ldapper.delete('uid=fake,ou=users,dc=acme,dc=org')
+    .catch(function(err) {
+      expect(err.name).to.be.equal('LDAPDeleteError');
+    });
+  });
+});
+*/
